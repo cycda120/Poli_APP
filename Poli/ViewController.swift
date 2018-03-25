8e9e4235ccd708836ee5c9f5a20ec875f2acda1b
陳小毛 <JohnnyChen@chenxiaomaode-macbook-pro.local>
2018-03-25 22:35:01 +0800

    Initial Commit

---
+++ Poli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poli
+//
+//  Created by 陳小毛 on 2018/3/25.
+//  Copyright © 2018年 陳小毛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
